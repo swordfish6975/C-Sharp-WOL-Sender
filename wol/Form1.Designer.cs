36b800e88b0cbd3a6882f12efd10b70b84a1cc8f
Steven <lasc175@mdte.com.au>
2015-06-24 15:34:50 +1000

    Initial Commit

---
+++ wol/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace wol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxMAC = new System.Windows.Forms.TextBox();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.txbPort = new System.Windows.Forms.TextBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.BGW = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxMAC
+            // 
+            this.tbxMAC.Location = new System.Drawing.Point(109, 43);
+            this.tbxMAC.Name = "tbxMAC";
+            this.tbxMAC.Size = new System.Drawing.Size(158, 20);
+            this.tbxMAC.TabIndex = 1;
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Location = new System.Drawing.Point(109, 17);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(158, 20);
+            this.tbxIP.TabIndex = 2;
+            // 
+            // txbPort
+            // 
+            this.txbPort.Location = new System.Drawing.Point(109, 69);
+            this.txbPort.Name = "txbPort";
+            this.txbPort.Size = new System.Drawing.Size(158, 20);
+            this.txbPort.TabIndex = 3;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 108);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.ReadOnly = true;
+            this.rtbOutput.Size = new System.Drawing.Size(284, 208);
+            this.rtbOutput.TabIndex = 4;
+            this.rtbOutput.Text = "";
+            // 
+            // BGW
+            // 
+            this.BGW.WorkerReportsProgress = true;
+            this.BGW.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGW_DoWork);
+            this.BGW.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGW_ProgressChanged);
+            this.BGW.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGW_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP/Hostname:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Mac Address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Port:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Router Setup";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.txbPort);
+            this.Controls.Add(this.tbxIP);
+            this.Controls.Add(this.tbxMAC);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WOL Sender";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxMAC;
+        private System.Windows.Forms.TextBox tbxIP;
+        private System.Windows.Forms.TextBox txbPort;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.ComponentModel.BackgroundWorker BGW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ wol/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace wol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Clear();
+            BGW.RunWorkerAsync();
+        }
+
+        private void BGW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IPAddress ipAddress;
+
+            try
+            {
+                IPAddress[] temp = Dns.GetHostAddresses(tbxIP.Text);
+                ipAddress = temp[0];
+
+                if (temp.Count() > 1)
+                    BGW.ReportProgress(15, "Multiple IP for DNS (using first one)");
+            }
+            catch
+            {
+                BGW.ReportProgress(10, "Failed to get IP from DNS");
+                return;
+            }
+            var response = WakeFunction(tbxMAC.Text, ipAddress, int.Parse(txbPort.Text));
+
+
+            if (response[0] == response[1])
+                BGW.ReportProgress(5, "Sent Magic Packet");
+            else
+                BGW.ReportProgress(10, "Only" + response[0] + " bytes of " + response[1] + "bytes sent");
+        }
+
+
+
+        private List<int> WakeFunction(string MAC_ADDRESS, IPAddress ipAddress, int port)
+        {
+
+            MAC_ADDRESS = MAC_ADDRESS.Replace(":","");
+            MAC_ADDRESS = MAC_ADDRESS.Replace("-", "");
+            System.Threading.Thread.Sleep(100);
+            BGW.ReportProgress(5, "Mac Address:" + MAC_ADDRESS);
+
+            if (MAC_ADDRESS.Length != 12)
+            {
+                BGW.ReportProgress(10, "Mac Address Incorrect Length (should be 12)");
+                return new List<int> { 0, 0 };
+            }
+            
+            System.Threading.Thread.Sleep(100);
+            BGW.ReportProgress(5, "Create Packet...");
+            int counter = 0;
+            //buffer to be send
+            byte[] bytes = new byte[1024];   // more than enough :-)
+            //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                bytes[counter++] = 0xFF;
+            //now repeate MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    bytes[counter++] =
+                        byte.Parse(MAC_ADDRESS.Substring(i, 2),
+                        NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
+
+            //now send wake up packet
+            System.Threading.Thread.Sleep(100);
+            BGW.ReportProgress(5, "Packet Made...");
+            System.Threading.Thread.Sleep(100);
+            BGW.ReportProgress(5, "About to send packet...");
+            System.Threading.Thread.Sleep(100);
+            return sendUDPMessage(ipAddress.ToString(), port, port, bytes);
+        }
+
+        public  List<int> sendUDPMessage(string ipAddress, int fromPort, int toPort, byte[] send_buffer)
+        {
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ipAddress), fromPort);
+
+            BGW.ReportProgress(5, "Remote Endpoint: " + endPoint.ToString());
+
+            UdpClient client = new UdpClient(toPort);
+            var sentBytes = client.Send(send_buffer, send_buffer.Length, endPoint);
+            client.Close();
+
+            System.Threading.Thread.Sleep(100);
+            return (new List<int> { sentBytes, send_buffer.Length });
+
+        }
+
+
+        private void BGW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == 5)
+            {
+                rtbOutput.SelectionColor = Color.Green;
+              
+            }
+            else if (e.ProgressPercentage == 10) //red
+            {
+                rtbOutput.SelectionColor = Color.Red;
+            }
+            else if (e.ProgressPercentage == 15) //orange
+            {
+                rtbOutput.SelectionColor = Color.Orange;
+            }
+
+            if (rtbOutput.Text == "")
+                rtbOutput.AppendText(e.UserState.ToString());
+            else
+                rtbOutput.AppendText(Environment.NewLine + e.UserState.ToString());
+        }
+
+        private void BGW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            rtbOutput.SelectionColor = Color.Green;
+            rtbOutput.AppendText(Environment.NewLine + "Done");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbxMAC.Text = Properties.Settings.Default.MAC;
+            tbxIP.Text =  Properties.Settings.Default.IP;
+            txbPort.Text = Properties.Settings.Default.Port;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.MAC = tbxMAC.Text;
+            Properties.Settings.Default.IP = tbxIP.Text;
+            Properties.Settings.Default.Port = txbPort.Text ;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("These instructions are writen for a particular router firmware, they may give you an idea how to configure your own. (step 3 is not required)");
+            Process.Start("http://www.dd-wrt.com/wiki/index.php/WOL#Remote_Wake_On_LAN_via_Port_Forwarding");
+        }
+
+    }
+
+  
+ 
+}
